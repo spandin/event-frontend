a0d4abda3fc419b6ce5d1c075ddb91482a747a0c
Stanislau <willstesi@gmail.com>
2024-04-22 22:44:02 +0300

    feat: add router provider

--- src/app/entryPoint.tsx
+++ src/app/entryPoint.tsx
@@ -1,14 +1,34 @@
+import './styles/_fonts.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from './theme'
 
 import RootLayout from './layouts/rootLayout'
+import LoginPage from '../pages/login'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <div>Error</div>,
+    children: [
+      {
+        path: 'login/',
+        element: <LoginPage />,
+        errorElement: <div>Error</div>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <RootLayout></RootLayout>
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 )
