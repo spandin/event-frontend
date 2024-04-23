5a96c1c6b4d051640f2e62d4cf6ddc4fe4c3c21e
Stanislau <willstesi@gmail.com>
2024-04-23 12:24:02 +0300

    feat: change global styles

--- src/app/theme/components/button.ts
+++ src/app/theme/components/button.ts
@@ -2,26 +2,31 @@ import { defineStyle, defineStyleConfig } from '@chakra-ui/react'
 
 const outline = defineStyle({
   h: 12,
+  px: 4,
   borderColor: '#e3e3e4',
   borderRadius: 10,
   border: '2px solid',
   fontSize: 'md',
-  fontWeight: 300,
-  fontFamily: 'Inter',
-  color: 'white'
+  fontWeight: 500,
+  color: 'black'
 })
 
 const solid = defineStyle({
   h: 12,
+  px: 4,
   borderRadius: 10,
   fontSize: 'md',
   fontWeight: 500,
-  fontFamily: 'Inter',
   background: 'brand.900',
   color: 'white',
 
   _hover: {
     background: 'brand.800',
+    color: 'white'
+  },
+
+  _active: {
+    background: 'brand.900',
     color: 'white'
   }
 })
