2e42fe50febebefd9ceb5e4698510182ab6b8bd8
Stanislau <willstesi@gmail.com>
2024-04-23 15:43:11 +0300

    feat: refactoring
    the router has been moved to another file

--- src/app/entryPoint.tsx
+++ src/app/entryPoint.tsx
@@ -2,28 +2,11 @@ import './styles/_fonts.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
 
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from './theme/_index'
-
-import RootLayout from './layouts/rootLayout'
-import LoginPage from '../pages/login'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    errorElement: <div>Error</div>,
-    children: [
-      {
-        path: 'login/',
-        element: <LoginPage />,
-        errorElement: <div>Error</div>
-      }
-    ]
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
