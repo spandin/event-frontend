bded3c74ec34c96606e8e9327035de36a04c71ed
Stanislau <willstesi@gmail.com>
2024-04-23 15:45:11 +0300

    feat: add app logo

--- src/shared/assets/icons/icons.ts
+++ src/shared/assets/icons/icons.ts
@@ -1,4 +1,5 @@
 export const ICONS = {
+  APP_LOGO: new URL('./logo.svg', import.meta.url).href,
   GOOGLE_LOGO: new URL('./google.svg', import.meta.url).href,
   APPLE_LOGO: new URL('./apple.svg', import.meta.url).href
 }
