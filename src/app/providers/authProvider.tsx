26850eaf6cdde8c1417889e8a21bc96d65de0ba7
bp26 <aleshak260006@gmail.com>
2024-04-25 14:25:42 +0300

    fix: fix isInitilized in AuthProvider

--- src/app/providers/authProvider.tsx
+++ src/app/providers/authProvider.tsx
@@ -1,21 +1,21 @@
 import { useAuth } from '@/shared/hooks'
 import { Center, Spinner } from '@chakra-ui/react'
-import { PropsWithChildren, useEffect, useRef } from 'react'
+import { PropsWithChildren, useEffect, useState } from 'react'
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
-  const isInitialized = useRef(false)
+  const [isInitialized, setIsInitilized] = useState(false)
   const { authenticate, isLoading } = useAuth()
 
   useEffect(() => {
     const initialize = async () => {
       await authenticate()
-      isInitialized.current = true
+      setIsInitilized(true)
     }
 
     initialize()
   }, [])
 
-  if (isLoading && !isInitialized.current) {
+  if (isLoading && !isInitialized) {
     return (
       <Center h="100dvh">
         <Spinner size="xl" />
