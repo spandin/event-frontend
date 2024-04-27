5e51e7070e0effe5badb3268b008042095b28071
Stanislau <willstesi@gmail.com>
2024-04-27 11:27:53 +0300

    feat: add change page transition

--- src/app/entryPoint.tsx
+++ src/app/entryPoint.tsx
@@ -1,17 +1,21 @@
-import './styles/_fonts.css'
+import './styles/_global.scss'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routerProvider'
-
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from './theme/_index'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <RouterProvider router={router} />
+      <RouterProvider
+        router={router}
+        future={{
+          v7_startTransition: true
+        }}
+      />
     </ChakraProvider>
   </React.StrictMode>
 )
