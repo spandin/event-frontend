0ca52ca7dd336df35be06ae6756e9af9a7cafcc4
Stanislau <willstesi@gmail.com>
2024-04-28 10:55:45 +0300

    feat: add new variant

--- src/app/theme/components/heading.ts
+++ src/app/theme/components/heading.ts
@@ -3,27 +3,38 @@ import { defineStyle, defineStyleConfig } from '@chakra-ui/react'
 const h1 = defineStyle({
   fontSize: '4xl',
   fontWeight: 500,
+  fontFamily: 'Jost, sans-serif',
   letterSpacing: '-0.02em'
 })
 
 const h2 = defineStyle({
   fontSize: '3xl',
   fontWeight: 500,
+  fontFamily: 'Jost, sans-serif',
   letterSpacing: '-0.02em'
 })
 
 const h3 = defineStyle({
   fontSize: '2xl',
   fontWeight: 500,
+  fontFamily: 'Jost, sans-serif',
   letterSpacing: '-0.02em'
 })
 
 const h4 = defineStyle({
   fontSize: 'xl',
   fontWeight: 500,
+  fontFamily: 'Jost, sans-serif',
+  letterSpacing: '-0.02em'
+})
+
+const h5 = defineStyle({
+  fontSize: 'lg',
+  fontWeight: 500,
+  fontFamily: 'Jost, sans-serif',
   letterSpacing: '-0.02em'
 })
 
 export const headingTheme = defineStyleConfig({
-  variants: { h1, h2, h3, h4 }
+  variants: { h1, h2, h3, h4, h5 }
 })
