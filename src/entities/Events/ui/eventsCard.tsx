2aa2faba97ed37f2d46db2ca6f0394d09b01275b
Stanislau <willstesi@gmail.com>
2024-04-28 18:24:13 +0300

    feat: add props and styling component

--- src/entities/Events/ui/eventsCard.tsx
+++ src/entities/Events/ui/eventsCard.tsx
@@ -1,41 +1,57 @@
+import { timestampToDate } from '@/shared/helpers/parseDate'
+import { Event } from '@/shared/types/events'
+import { CustomLink as Link } from '@/shared/ui'
 import {
   Card,
-  Heading,
   HStack,
   Text,
   CardHeader,
   CardFooter,
   AvatarGroup,
   Avatar,
-  AvatarBadge
+  AvatarBadge,
+  Heading
 } from '@chakra-ui/react'
 
-export const EventsCard = () => {
+export const EventsCard = ({ event }: { event: Event }) => {
   return (
     <Card
       w={'full'}
       p={4}
       borderColor={'lightStroke'}
-      borderWidth={'2px'}
+      borderWidth={event.isActive ? '0px' : '2px'}
       shadow={'none'}
       rounded={20}
       gap={4}
+      bg={event.isActive ? 'darkBrand' : 'transparent'}
+      color={event.isActive ? 'white' : 'black'}
+      _hover={{
+        bg: event.isActive ? 'brand.900' : 'lightHover'
+      }}
+      transition={'background-color 0.3s ease-in-out'}
     >
       <CardHeader w={'full'} p={0}>
         <HStack w={'full'} justify={'space-between'}>
-          <Heading variant={'h5'}>Ресторан Ember</Heading>
-          <Text fontSize={'sm'} color={'gray.500'}>
-            24.05.2024
+          <Link to={`/events/${event.id}`}>
+            <Heading variant={'h5'}>{event.title}</Heading>
+          </Link>
+
+          <Text fontSize={'sm'} color={event.isActive ? 'gray.200' : 'black'}>
+            {timestampToDate(event.date)}
           </Text>
         </HStack>
-        <Text fontSize={'sm'} color={'gray.500'} alignSelf={'flex-start'}>
-          просп. Победителей, 9, Минск (этаж 7)
+        <Text
+          fontSize={'sm'}
+          color={event.isActive ? 'gray.200' : 'black'}
+          alignSelf={'flex-start'}
+        >
+          {event.location}
         </Text>
       </CardHeader>
 
       <CardFooter w={'full'} p={0}>
         <HStack w={'full'} justify={'space-between'}>
-          <AvatarGroup size="sm" max={2}>
+          <AvatarGroup color={'darkBrand'} size="sm" max={2}>
             <Avatar name="Segun Adebayo" bg={'brand.900'} />
             <Avatar name="Ryan Florence" src="https://bit.ly/ryan-florence">
               <AvatarBadge boxSize={3} bg="green.500" />
@@ -46,10 +62,16 @@ export const EventsCard = () => {
           </AvatarGroup>
 
           <HStack gap={2}>
-            <Text fontWeight={500}>130BYN</Text>
-            <Text fontWeight={500} color={'orange'}>
-              33%
-            </Text>
+            {event.isActive ? (
+              <Text>активная</Text>
+            ) : (
+              <>
+                <Text fontWeight={500}>{event.totalCost}BYN</Text>
+                <Text fontWeight={500} color={'orange'}>
+                  {event.myPercentage}%
+                </Text>
+              </>
+            )}
           </HStack>
         </HStack>
       </CardFooter>
