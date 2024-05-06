57f1903c0452259e2610fdf5536dd3e91cdbae5a
Stanislau <willstesi@gmail.com>
2024-05-06 18:42:15 +0300

    feat: create keyframes

---
+++ src/shared/styles/keyframes.ts
@@ -0,0 +1,7 @@
+import { keyframes } from '@chakra-ui/react'
+
+export const gradientAnimation = keyframes`
+  0% { background-position: 0% 50%; }
+  50% { background-position: 100% 50%; }
+  100% { background-position: 0% 50%; }
+`
