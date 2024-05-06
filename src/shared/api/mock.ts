baec008840fdfaa06dafef4e5ef83bd264c4dfa3
Stanislau <willstesi@gmail.com>
2024-05-06 18:37:02 +0300

    feat: change name

---
+++ src/shared/api/mock.ts
@@ -0,0 +1,71 @@
+import { Event } from '../types/events'
+
+export const events: Event[] = [
+  {
+    id: '1',
+    title: 'Ресторан Ember',
+    location: 'Минск',
+    date: '2024-05-06T16:30:33.000Z',
+    totalCost: 120,
+    myPercentage: 33,
+    isActive: true
+  },
+  {
+    id: '2',
+    title: 'Кинотеатр',
+    location: 'Могилев',
+    date: '2024-05-12T12:12:00.000Z',
+    totalCost: 40,
+    myPercentage: 50,
+    isActive: false
+  },
+  {
+    id: '3',
+    title: 'Страйкбол',
+    location: 'Пинск',
+    date: '2024-04-15T12:20:30.000Z',
+    totalCost: 80,
+    myPercentage: 100,
+    isActive: false
+  },
+  {
+    id: '4',
+    title: 'Аквапарк',
+    location: 'Солигорск',
+    date: '2024-06-05T12:13:45.000Z',
+    totalCost: 80,
+    myPercentage: 100,
+    isActive: false
+  },
+  {
+    id: '5',
+    title: 'Кальян',
+    location: 'Бобруйск',
+    date: '2024-05-28T18:21:30.000Z',
+    totalCost: 160,
+    myPercentage: 40,
+    isActive: false
+  },
+  {
+    id: '6',
+    title: 'Театр',
+    location: 'Брест',
+    date: '2024-05-22T18:20:00.000Z',
+    totalCost: 220,
+    myPercentage: 100,
+    isActive: false
+  }
+]
+
+export const members = [
+  {
+    id: '1a',
+    name: 'Stanislau',
+    img: 'https://avatars.mds.yandex.net/i?id=c3af4cc2793f2182167ea914542aa50707833a19-12497202-images-thumbs&n=13'
+  },
+  {
+    id: '1b',
+    name: 'Nikita',
+    img: 'https://avatars.mds.yandex.net/i?id=1d3d7556d577914e7bea3a3984f248bfe336afab-12939114-images-thumbs&n=13'
+  }
+]
