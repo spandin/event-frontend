aa133a5a7ca98b5c60ab8e01c445adb7e37a3c1b
Stanislau <willstesi@gmail.com>
2024-05-11 14:23:17 +0300

    feat: refactoring

--- src/widgets/Events/membersList.tsx
+++ src/widgets/Events/membersList.tsx
@@ -27,77 +27,76 @@ export const MembersList = () => {
   })
 
   return (
-    <VStack w={'full'}>
-      <VStack
-        w={'full'}
-        p={4}
-        rounded={20}
-        gap={4}
-        bg={'linear-gradient(135deg, #0866F8, #3A1891)'}
-        bgSize={'10% 100%'}
-        animation={`${gradientAnimation} 20s ease infinite`}
-      >
-        <HStack w={'full'} justify={'space-between'} align={'center'}>
-          <Heading color={'white'} variant={'h3'}>
-            Участники
-          </Heading>
+    <VStack
+      w={'full'}
+      p={4}
+      rounded={20}
+      gap={4}
+      bg={'linear-gradient(135deg, #0866F8, #3A1891)'}
+      bgSize={'20% 100%'}
+      boxShadow="inset 0 0 4px rgba(0, 0, 0, 0.2)"
+      animation={`${gradientAnimation} 20s ease infinite`}
+    >
+      <HStack w={'full'} justify={'space-between'} align={'center'}>
+        <Heading color={'white'} variant={'h3'}>
+          Участники
+        </Heading>
 
-          <AddMemberButton />
-        </HStack>
+        <AddMemberButton />
+      </HStack>
 
-        <VStack w={'full'} gap={4}>
-          <AnimatePresence>
-            {sortedMembersByAlphabetic.slice(0, visibleCards).map((member, index) => (
-              <motion.div
-                key={index}
-                initial={{ opacity: 0, y: -50, width: '100%' }}
-                animate={{ opacity: 1, y: 0, width: '100%' }}
-                exit={{ opacity: 0, y: -500 }}
-                transition={{ duration: 0.3, delay: index * 0.1 }}
-              >
-                {member.isOwner ? (
-                  <HStack key={member.id} w={'full'}>
-                    <MemberCard member={member} />
-                  </HStack>
-                ) : (
-                  <HStack key={member.id} w={'full'}>
-                    <MemberCard member={member} /> <DeleteMemberButton member={member} />
-                  </HStack>
-                )}
-              </motion.div>
-            ))}
-          </AnimatePresence>
-
-          {members.length >= visibleCards && (
-            <Text color={'gray.100'} fontFamily={'Jost'} fontWeight={400} fontSize={'sm '}></Text>
-          )}
-        </VStack>
-        <HStack w={'full'} justify={'flex-start'}>
-          {visibleCards >= members.length && (
-            <Button
-              w={'full'}
-              h={10}
-              variant={'solidDark'}
-              rounded={'full'}
-              onClick={() => setVisibleCards(initialLengthCards)}
+      <VStack w={'full'} gap={4}>
+        <AnimatePresence>
+          {sortedMembersByAlphabetic.slice(0, visibleCards).map((member, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, y: -50, width: '100%' }}
+              animate={{ opacity: 1, y: 0, width: '100%' }}
+              exit={{ opacity: 0, y: -100 }}
+              transition={{ duration: 0.3, delay: index * 0.05 }}
             >
-              Свернуть
-            </Button>
-          )}
+              {member.isOwner ? (
+                <HStack key={member.id} w={'full'}>
+                  <MemberCard member={member} />
+                </HStack>
+              ) : (
+                <HStack key={member.id} w={'full'}>
+                  <MemberCard member={member} /> <DeleteMemberButton member={member} />
+                </HStack>
+              )}
+            </motion.div>
+          ))}
+        </AnimatePresence>
 
-          {visibleCards < members.length && (
-            <Button
-              w={'full'}
-              h={10}
-              variant={'solidDark'}
-              rounded={'full'}
-              onClick={loadMoreMembers}
-            >
-              Ещё +{members.length - visibleCards}
-            </Button>
-          )}
-        </HStack>
+        {members.length >= visibleCards && (
+          <Text color={'gray.100'} fontFamily={'Jost'} fontWeight={400} fontSize={'sm '}></Text>
+        )}
       </VStack>
+      <HStack w={'full'} justify={'flex-start'}>
+        {visibleCards >= members.length && (
+          <Button
+            w={'full'}
+            h={10}
+            variant={'solidDark'}
+            rounded={'full'}
+            onClick={() => setVisibleCards(initialLengthCards)}
+          >
+            Свернуть
+          </Button>
+        )}
+
+        {visibleCards < members.length && (
+          <Button
+            w={'full'}
+            h={10}
+            variant={'solidDark'}
+            rounded={'full'}
+            onClick={loadMoreMembers}
+          >
+            Ещё +{members.length - visibleCards}
+          </Button>
+        )}
+      </HStack>
     </VStack>
   )
 }
